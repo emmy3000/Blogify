d23045b1e5ed2beb11b10c2269b945aab43fa096
Emeka Emodi <emodiemeka@gmail.com>
2023-12-18 07:40:33 +0100

    feat: Define function to load user by user ID

---
+++ blogify_app/models.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+from blogify_app import db, login_manager
+from flask_login import UserMixin
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    """
+    Load a user by user ID.
+
+    Args:
+        user_id (int): The ID of the user to load.
+
+    Returns:
+        User: The User object associated with the given ID.
+    """
+    return User.query.get(int(user_id))
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpeg')
+    password = db.Column(db.String(120), nullable=False)
+    posts = db.relationship('Post', backref='author', lazy=True)
+
+    def __repr__(self):
+        return 'User("{}", "{}", "{}")'.format(self.username, self.email, self.image_file)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return 'Post("{}", "{}")'.format(self.title, self.date_posted)
