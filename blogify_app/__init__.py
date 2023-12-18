0f42f1784f31420fd0b56cb9f900b09e19c302da
Emeka Emodi <emodiemeka@gmail.com>
2023-12-18 08:14:43 +0100

    feat: Initialize Flask app with extensions and routes

---
+++ blogify_app/__init__.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+# Create a Flask application instance
+app = Flask(__name__)
+
+# Set the secret key for securing session data
+app.config['SECRET_KEY'] = '4f4361ae36d8a09864390826f3f8fe4e'
+
+# Set the URI for the SQLite database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+# Create a SQLAlchemy database instance
+db = SQLAlchemy(app)
+
+# Create a Bcrypt instance for password hashing
+bcrypt = Bcrypt(app)
+
+# Create a LoginManager instance for handling user login
+login_manager = LoginManager(app)
+
+# Set the view to redirect to for login if the user is not logged in
+login_manager.login_view = 'login'
+
+# Set the category for displaying login messages
+login_manager.login_message_category = 'info'
+
+# Import the routes module after initializing the app and extensions
+from blogify_app import routes
