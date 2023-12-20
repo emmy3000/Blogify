f7396074f6d84ada46e570478b1c589f827f3406
Emeka Emodi <emodiemeka@gmail.com>
2023-12-20 08:13:44 +0100

    feat:Initialize Flask app and extensions

--- blogify_app/__init__.py
+++ blogify_app/__init__.py
@@ -1,32 +1,24 @@
-#!/usr/bin/env python3
+"""
+Initialize the Flask application and its extensions.
+"""
 
+from dotenv import load_dotenv
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from config import Config
 
-# Create a Flask application instance
+# Load environment variables from .env file
+load_dotenv()
+
 app = Flask(__name__)
+app.config.from_object(Config)
 
-# Set the secret key for securing session data
-app.config['SECRET_KEY'] = '4f4361ae36d8a09864390826f3f8fe4e'
-
-# Set the URI for the SQLite database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-
-# Create a SQLAlchemy database instance
 db = SQLAlchemy(app)
-
-# Create a Bcrypt instance for password hashing
 bcrypt = Bcrypt(app)
-
-# Create a LoginManager instance for handling user login
 login_manager = LoginManager(app)
-
-# Set the view to redirect to for login if the user is not logged in
 login_manager.login_view = 'login'
-
-# Set the category for displaying login messages
 login_manager.login_message_category = 'info'
 
 # Import the models module after initializing the app and extensions
