ca5b463a3ef4180afbcf2fc38a931d53d2b7401b
Emeka Emodi <emodiemeka@gmail.com>
2023-12-20 08:39:24 +0100

    feat:Initialize Flask app and extensions

--- blogify_app/__init__.py
+++ blogify_app/__init__.py
@@ -7,7 +7,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
-from config import Config
+from .config import Config
 
 # Load environment variables from .env file
 load_dotenv()
