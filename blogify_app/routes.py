6412c4f29d02b94d8cf404049f841d960b910e1d
Emeka Emodi <emodiemeka@gmail.com>
2023-12-29 03:58:55 +0100

    feat: Integrated 'Flask-Mail' functionalities for email sending; included logic for sending password reset emails

--- blogify_app/routes.py
+++ blogify_app/routes.py
@@ -90,13 +90,11 @@ def register():
         return redirect(url_for("home"))
     form = RegistrationForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt.\
-            generate_password_hash(form.password.data)\
-            .decode("utf-8")
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
         user = User(
-            username=form.username.data,
-            email=form.email.data,
-            password=hashed_password
+            username=form.username.data, email=form.email.data, password=hashed_password
         )
         db.session.add(user)
         db.session.commit()
@@ -181,10 +179,7 @@ def save_picture(form_picture):
     random_hex = secrets.token_hex(8)
     _, f_ext = os.path.splitext(form_picture.filename)
     picture_fn = random_hex + f_ext
-    picture_path = os.path.join(app.root_path,
-                                "static/profile_pics",
-                                picture_fn
-                                )
+    picture_path = os.path.join(app.root_path, "static/profile_pics", picture_fn)
     form_picture.save(picture_path)
     # Resize picture if >125 pixels
     output_size = (125, 125)
@@ -499,9 +494,9 @@ def reset_token(token):
         return redirect(url_for("reset_request"))
     form = ResetPasswordForm()
     if form.validate_on_submit():
-        hashed_password = bcrypt\
-            .generate_password_hash(form.password.data)\
-            .decode("utf-8")
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode(
+            "utf-8"
+        )
         user.password = hashed_password
         db.session.commit()
         flash(
